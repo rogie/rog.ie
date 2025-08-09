82b73b664e20266814327bec1ad6ba2d379581a4
Rogie King <hi@rog.ie>
2025-08-09 14:00:40 -0600

    deleting

--- Figma/Plugins & Widgets/Noise & Texture/ui-src/download-json.js
+++
@@ -1,9 +0,0 @@
-function downloadJSON(json, name = 'download.json') {
-    let link = document.createElement('a')
-    let stringified = JSON.stringify(json, null, 4)
-    link.setAttribute('href', `data:text/json;charset=utf-8,${encodeURIComponent(stringified)}`)
-    link.setAttribute('download', name)
-    link.click()
-    return;
-}
-export default downloadJSON;
